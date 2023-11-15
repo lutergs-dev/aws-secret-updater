13287aa983498ec02ad51b8ec662c41b2fd46131
LuterGS <koo04034@gmail.com>
2023-11-15 18:57:49 +0900

    multi-stage build for fast access to curl

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,26 @@
+FROM alpine AS Downloader
+
+RUN apk --no-cache add curl unzip
+
+WORKDIR /
+
+RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip" && \
+    unzip awscliv2.zip && \
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
+
 FROM ubuntu:22.04
 
-COPY ./run.sh ~/run.sh
+COPY ./run.sh /root/run.sh
+COPY --from=Downloader /aws /root/aws
+COPY --from=Downloader /kubectl /root/kubectl
 
-WORKDIR ~/
+WORKDIR /root
 
-RUN apt-get update &&  \
-    apt install curl unzip -y && \
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip" && \
-    unzip awscliv2.zip && \
-    ./aws/install && \
-    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl" && \
-    chmod 755 ~/run.sh
+RUN ./aws/install && \
+    chmod 755 /root/run.sh && \
+    chmod 755 /root/kubectl
 
-CMD ["/bin/bash", "~/run.sh"]
+CMD ["/bin/bash", "/root/run.sh"]
 
 
 
--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 # create aws credential folder and set aws info
-mkdir -p ~/.aws
-echo "[default]" > ~/.aws/config
-echo "region = $REGION" >> ~/.aws/config
-echo "[default]" > ~/.aws/credentials
-echo "aws_access_key_id = $ACCESS_KEY" >> ~/.aws/credentials
-echo "aws_secret_access_key = $SECRET_KEY" >> ~/.aws/credentials
+mkdir -p /root/.aws
+echo "[default]" > /root/.aws/config
+echo "region = $REGION" >> /root/.aws/config
+echo "[default]" > /root/.aws/credentials
+echo "aws_access_key_id = $ACCESS_KEY" >> /root/.aws/credentials
+echo "aws_secret_access_key = $SECRET_KEY" >> /root/.aws/credentials
 
-aws ecr get-login-password --region $REGION > ~/key.txt
+aws ecr get-login-password --region $REGION > /root/key.txt
 
-chmod 755 kubectl
-
-~/kubectl --kubeconfig=/etc/secret/config create secret docker-registry $SECRET_NAME \
+/root/kubectl --kubeconfig=/etc/secret/config delete secret $SECRET_NAME \
+  -n $NAMESPACE
+/root/kubectl --kubeconfig=/etc/secret/config create secret docker-registry $SECRET_NAME \
   --docker-server=$REPOSITORY_URL \
   --docker-username=AWS \
-  --docker-password=$(cat ~/key.txt) \
+  --docker-password=$(cat /root/key.txt) \
   -n $NAMESPACE
